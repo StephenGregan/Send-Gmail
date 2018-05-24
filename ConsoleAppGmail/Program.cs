c34342ccecfb296ad2a5b6c5b6355607a65e60ae
StephenGregan <grego@live.ie>
2018-05-24 14:26:00 +0100

    initial commit

---
+++ ConsoleAppGmail/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mainBodyHtml = "This is a test";
+            var msg = new MailMessage("", "", "Hi there", mainBodyHtml);
+            msg.To.Add("");
+            msg.IsBodyHtml = true;
+            var smptClient = new SmtpClient("smpt.gmail.com", 587);
+            smptClient.Credentials = new NetworkCredential("","");
+            smptClient.EnableSsl = true;
+            smptClient.Send(msg);
+            Console.WriteLine("Email sent");
+        }
+    }
+}
